67090e1a6df458986a171348745b332efd4ca058
HuaxuanGAO <33063662+HuaxuanGAO@users.noreply.github.com>
2019-11-24 18:07:22 +0800

    for input more than 512 in len

--- sha-1.py
+++ sha-1.py
@@ -19,16 +19,37 @@ def int2bstr(k):
     #  integer k to n bit string
     return "{0:32b}".format(k).replace(" ", "0")
 
-def splitChunk(bstr):
-    return [bstr[i:i+32] for i in range(0, len(bstr), 32)]
-
 def leftRotate(bstr, n):
     return bstr[n:]+bstr[0:n]
 
 def negate(bstr):
     return int2bstr(int(bstr,2) ^ (2**32-1))
 
-def extendChunks(chunks):
+def preprocess(text):
+    ascii = text2ASCII(text)
+    print("to ascii \n%s"%(ascii))
+
+    binary = ASCII2binary(ascii)
+    print("to binary \n%s"%(binary))
+
+    bstr = "".join(binary)+"1"
+    mod = mod512448(bstr)
+    print("pad until mod 512 == 448\n%s"%(mod))
+
+    len_binary = length2binary(bstr)
+    print("length to binary\n%s"%(len_binary))
+
+    bstr = mod + len_binary
+    print("join and pad 1 and length \n%s"%(bstr))
+    return bstr
+
+def splitChunks(bstr):
+    return [bstr[i:i+512] for i in range(0, len(bstr), 512)]
+
+def splitWords(bstr):
+    return [bstr[i:i+32] for i in range(0, len(bstr), 32)]
+
+def extendWords(chunks):
     i = len(chunks)
     while i < 80:
         a = chunks[i-3]
@@ -68,32 +89,6 @@ def bitOp4(B, C, D):
     return int2bstr(F), K
 
 def SHA1(text):
-    ascii = text2ASCII(text)
-    print("to ascii \n%s"%(ascii))
-
-    binary = ASCII2binary(ascii)
-    print("to binary \n%s"%(binary))
-
-    bstr = "".join(binary)+"1"
-    mod = mod512448(bstr)
-    print("pad until mod 512 == 448\n%s"%(mod))
-
-    len_binary = length2binary(bstr)
-    print("length to binary\n%s"%(len_binary))
-
-    bstr = mod + len_binary
-    print("join and pad 1 and length \n%s"%(bstr))
-
-    print(len(bstr))
-    chunks = splitChunk(bstr)
-    print("split chunks")
-    for i, chunk in enumerate(chunks):
-        print("chunk %d: %s"%(i, chunk))
-
-    chunks = extendChunks(chunks)
-    print("extend chunks")
-    for i, chunk in enumerate(chunks):
-        print("chunk %d: %s"%(i, chunk))
 
     h0 = "01100111010001010010001100000001"
     h1 = "11101111110011011010101110001001"
@@ -105,53 +100,69 @@ def SHA1(text):
     C = h2
     D = h3
     E = h4
-    for i, chunk in enumerate(chunks):
-        if i < 20:
-            F, K = bitOp1(B, C, D)
-        elif i < 40:
-            F, K = bitOp2(B, C, D)
-        elif i < 60:
-            F, K = bitOp3(B, C, D)
-        elif i < 80:
-            F, K = bitOp4(B, C, D)
-        # (A left rotate 5) + F + E + K + (the current word)
-        alrt = leftRotate(A, 5)
-        print("K %s"%(K))
-        print("ALROT5 %s"%(alrt))
-        print("F %s"%(F))
-        temp = int2bstr(int(alrt,2)+ int(F,2) +int(E,2)+int(K,2)+int(chunk,2))
-        # keep right most 32
-        temp = temp[-32:]
-        print("iter %d, temp %s"%(i, temp))
-        E = D
-        D = C
-        C = leftRotate(B, 30)
-        B = A
-        A = temp
-        print("A %s"%A)
-        print("B %s"%B)
-        print("C %s"%C)
-        print("D %s"%D)
-        print("E %s"%E)
 
+    bstr = preprocess(text)
+    print(len(bstr))
+    chunks = splitChunks(bstr)
+    for chunk in chunks:
+        words = splitWords(chunk)
+        print("split words")
+        for i, word in enumerate(words):
+            print("word %d: %s"%(i, word))
 
-    h0 = int2bstr(int(h0,2)+int(A,2))
-    print("h0 %s"%(h0))
-    h1 = int2bstr(int(h1,2)+int(B,2))
-    print("h1 %s"%(h1))
-    h2 = int2bstr(int(h2,2)+int(C,2))
-    print("h2 %s"%(h2))
-    h3 = int2bstr(int(h3,2)+int(D,2))
-    print("h3 %s"%(h3))
-    h4 = int2bstr(int(h4,2)+int(E,2))
-    print("h4 %s"%(h4))
+        words = extendWords(words)
+        print("extend words")
+        for i, word in enumerate(words):
+            print("word %d: %s"%(i, word))
+    
+        for i, word in enumerate(words):
+            if i < 20:
+                F, K = bitOp1(B, C, D)
+            elif i < 40:
+                F, K = bitOp2(B, C, D)
+            elif i < 60:
+                F, K = bitOp3(B, C, D)
+            elif i < 80:
+                F, K = bitOp4(B, C, D)
+            # (A left rotate 5) + F + E + K + (the current word)
+            alrt = leftRotate(A, 5)
+            print("K %s"%(K))
+            print("ALROT5 %s"%(alrt))
+            print("F %s"%(F))
+            temp = int2bstr(int(alrt,2)+ int(F,2) +int(E,2)+int(K,2)+int(word,2))
+            # keep right most 32
+            temp = temp[-32:]
+            print("iter %d, temp %s"%(i, temp))
+            E = D
+            D = C
+            C = leftRotate(B, 30)
+            B = A
+            A = temp
+            print("A %s"%A)
+            print("B %s"%B)
+            print("C %s"%C)
+            print("D %s"%D)
+            print("E %s"%E)
+
+        h0 = int2bstr(int(h0,2)+int(A,2))
+        print("h0 %s"%(h0))
+        h1 = int2bstr(int(h1,2)+int(B,2))
+        print("h1 %s"%(h1))
+        h2 = int2bstr(int(h2,2)+int(C,2))
+        print("h2 %s"%(h2))
+        h3 = int2bstr(int(h3,2)+int(D,2))
+        print("h3 %s"%(h3))
+        h4 = int2bstr(int(h4,2)+int(E,2))
+        print("h4 %s"%(h4))
 
     return '{:08x}'.format(int(h0[-32:], 2)) + '{:08x}'.format(int(h1[-32:], 2)) + '{:08x}'.format(int(h2[:-32], 2))\
          + '{:08x}'.format(int(h3[-32:], 2)) + '{:08x}'.format(int(h4[-32:], 2))
 
-text = "this is great!"
+# text = "this is great!"
+with open('test.txt', 'r') as myfile:
+  text = myfile.read()
 sha = SHA1(text)
 print(text)
 print(sha)
 
-# 1deef8461ffb4b4400000001e3813d17a715aa3a
+# 98bb2b7d040eae5b98badcfe103254768f2a2e86
